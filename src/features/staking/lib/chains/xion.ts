01e1a6758d2faab923393a8db4b4d7ccfb081a4c
2xburnt <169301814+2xburnt@users.noreply.github.com>
2025-03-04 13:36:12 -0600

    fix errors

--- src/features/staking/lib/chains/xion.ts
+++ src/features/staking/lib/chains/xion.ts
@@ -2,7 +2,9 @@ import xionMainnetChainAssets from "chain-registry/mainnet/xion/assets";
 import xionMainnetChainInfo from "chain-registry/mainnet/xion/chain";
 import xionTestnetChainAssets from "chain-registry/testnet/xiontestnet/assets";
 import xionTestnetChainInfo from "chain-registry/testnet/xiontestnet/chain";
+import xionTestnet2ChainAssets from "chain-registry/testnet/xiontestnet2/assets";
+import xionTestnet2ChainInfo from "chain-registry/testnet/xiontestnet2/chain";
 
 // @todo Add testnet-2 here when the times comes
-export const assets = [xionMainnetChainAssets, xionTestnetChainAssets];
-export const chains = [xionMainnetChainInfo, xionTestnetChainInfo];
+export const assets = [xionMainnetChainAssets, xionTestnetChainAssets, xionTestnet2ChainAssets];
+export const chains = [xionMainnetChainInfo, xionTestnetChainInfo, xionTestnet2ChainInfo];
